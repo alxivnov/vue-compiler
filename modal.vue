732a3bd81b1634c67614531ed56eb2c51078e18d
Alexander Ivanov <alex.p.ivanov@gmail.com>
2019-10-16 00:05:32 +0300

    Added modal

---
+++ modal.vue
@@ -0,0 +1,79 @@
+<template functional>
+	<transition appear @appear="
+		$('#' + (data.attrs.id || 'modal'))
+			.on('show.bs.modal', function (e) {
+				if (listeners.show)
+					listeners.show(e);
+			})
+			.on('shown.bs.modal', function (e) {
+				if (listeners.shown)
+					listeners.shown(e);
+			})
+			.on('hide.bs.modal', function (e) {
+				if (listeners.hide)
+					listeners.hide(e);
+			})
+			.on('hidden.bs.modal', function (e) {
+				if (listeners.hidden)
+					listeners.hidden(e);
+			})
+	">
+		<div
+			:class="{
+				'modal': true,
+				'fade': props.fade !== undefined && props.fade !== false
+			}"
+			tabindex="-1"
+			role="dialog"
+
+			:aria-labelledby="(data.attrs.id || 'modal') + '-label'"
+			aria-hidden="true"
+			
+			v-bind="data.attrs"
+
+			:show="$('#' + (data.attrs.id || 'modal')).modal(props.show ? 'show' : 'hide') "
+		>
+			<div
+				:class="{
+					'modal-dialog': true,
+					'modal-dialog-scrollable': props.scrollable !== undefined && props.scrollable !== false,
+					'modal-dialog-centered': props.centered !== undefined && props.centered !== false,
+
+					'modal-sm': props.size == 'sm',
+					'modal-lg': props.size == 'lg',
+					'modal-xl': props.size == 'xl',
+				}"
+				role="document"
+			>
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" :id="(data.attrs.id || 'modal') + '-label'">{{ props.title }}</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<!--<span aria-hidden="true">-->&times;<!--</span>-->
+						</button>
+					</div>
+					<div class="modal-body">
+						<slot></slot>
+					</div>
+					<div class="modal-footer">
+						<slot name="footer"></slot>
+					</div>
+				</div>
+			</div>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+	props: [
+		'title',
+		'scrollable',			// FALSE|true
+		'centered',				// FALSE|true
+		'fade',					// FALSE|true
+		'size',					// sm|UNDEFINED|lg|xl
+
+		'show',					// FALSE|true
+	]
+}
+</script>
